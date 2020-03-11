b9754f55994d251cc2644883fecf254c84eae2f8
Rob Spears <rob.sp3ars@gmail.com>
2020-03-11 13:40:17 -0400

    Implement decorator function that captures and displays profiling measurements of original algorithm

--- tuneup.py
+++ tuneup.py
@@ -15,7 +15,17 @@ def profile(func):
     # You need to understand how decorators are constructed and used.
     # Be sure to review the lesson material on decorators, they are used
     # extensively in Django and Flask.
-    raise NotImplementedError("Complete this decorator function")
+    @functools.wraps(func)
+    def pro_wrapper(*args, **kwargs):
+        timeit_helper()
+        pro = cProfile.Profile()
+        pro.enable()
+        duplicates = func(*args, **kwargs)
+        pro.disable()
+        stats = pstats.Stats(pro)
+        stats.strip_dirs().sort_stats('cumulative').print_stats()
+        return duplicates
+    return pro_wrapper
 
 
 def read_movies(src):
@@ -33,6 +43,7 @@ def is_duplicate(title, movies):
     return False
 
 
+@profile
 def find_duplicate_movies(src):
     """Returns a list of duplicate movies from a src list"""
     movies = read_movies(src)
@@ -49,31 +60,30 @@ def timeit_helper():
     number = 5
     repeat = 7
     t = timeit.Timer(stmt="find_duplicate_movies('movies.txt')",
-                     setup='''
-                    def read_movies(src):
-                         """Returns a list of movie titles"""
-                        print('Reading file: {}'.format(src))
-                        with open(src, 'r') as f:
-                            return f.read().splitlines()
+                     setup='''def read_movies(src):
+    """Returns a list of movie titles"""
+    with open(src, 'r') as f:
+        return f.read().splitlines()
 
 
-                    def is_duplicate(title, movies):
-                        """returns True if title is within movies list"""
-                        for movie in movies:
-                        if movie.lower() == title.lower():
-                            return True
-                        return False
+def is_duplicate(title, movies):
+    """returns True if title is within movies list"""
+    for movie in movies:
+        if movie.lower() == title.lower():
+            return True
+    return False
 
 
-                    def find_duplicate_movies(src):
-                        """Returns a list of duplicate movies from a src list"""
-                        movies=read_movies(src)
-                        duplicates=[]
-                        while movies:
-                            movie=movies.pop()
-                            if is_duplicate(movie, movies):
-                            duplicates.append(movie)
-                        return duplicates")''')
+
+def find_duplicate_movies(src):
+    """Returns a list of duplicate movies from a src list"""
+    movies = read_movies(src)
+    duplicates = []
+    while movies:
+        movie = movies.pop()
+        if is_duplicate(movie, movies):
+            duplicates.append(movie)
+    return duplicates''')
     results = t.repeat(number=number, repeat=repeat)
     min_time = min([res/number for res in results])
     print('Best time across {} repeats of {} repeats: {}'.format(
